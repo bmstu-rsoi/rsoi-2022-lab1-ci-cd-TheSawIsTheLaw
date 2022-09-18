c2df278f68de060aa4801a526e02b7c1ced1161c
thesawisthelaw <dimakrok@bk.ru>
2022-09-18 21:58:06 +0300

    ADD: persons mapping

--- ShittyTemplate/shittyTemplate/src/main/kotlin/ru/shit/shittytemplate/controller/PersonsController.kt
+++ ShittyTemplate/shittyTemplate/src/main/kotlin/ru/shit/shittytemplate/controller/PersonsController.kt
@@ -1,7 +1,8 @@
 package ru.shit.shittytemplate.controller
-
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
+import ru.shit.shittytemplate.entity.Person
 
 @Controller
 @RequestMapping("/api/v1/persons")
@@ -10,12 +11,30 @@ class PersonsController {
     @GetMapping("", "/")
     @ResponseBody
     fun getPersons(): String {
-        return "Just lol"
+        return "Just lol" // 200
     }
 
     @GetMapping("/{id}")
     @ResponseBody
     fun getPerson(@PathVariable id: Int): String {
-        return "Lol, it's $id"
+        return "Lol, it's $id" // 200 404
+    }
+
+    @PostMapping("", "/")
+    @ResponseStatus
+    fun createPerson(@RequestBody person: Person): ResponseStatus {
+        return ResponseStatus(HttpStatus.CREATED) // 201 400
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus
+    fun deletePerson(@PathVariable id: Int): ResponseStatus {
+        return ResponseStatus(HttpStatus.NO_CONTENT)
+    }
+
+    @PatchMapping("/{id}")
+    @ResponseStatus
+    fun patchPerson(@PathVariable id: Int): ResponseStatus {
+        return ResponseStatus(HttpStatus.OK) // 404 400
     }
 }
